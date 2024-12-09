7a7af9538acd0c002e814f78f6c0457287e613e3
trentwiles <wilest7@gmail.com>
2024-12-09 12:32:22 -0500

    rename

--- directory.h
+++ directory.h
@@ -1,29 +1,28 @@
-// based on cs3650 starter code
+// Directory manipulation functions.
+//
+// Feel free to use as inspiration. Provided as-is.
 
+// Based on cs3650 starter code
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
-// max dir name size, CHANGE LATER
-#define DIR_NAME 48
+#define DIR_NAME_LENGTH 48
 
-#include "slist.h"
 #include "blocks.h"
 #include "inode.h"
+#include "slist.h"
 
 typedef struct dirent {
-    char name[DIR_NAME];// The name of the directory
-    int  inum; // the number of the inode in the inode table?
-    char used; // is this directory entry being used?
-    char _reserved[11]; // round it out to 64B
-} dirent;
+  char name[DIR_NAME_LENGTH];
+  int inum;
+  char _reserved[12];
+} dirent_t;
 
-void directory_init(); 
-int directory_lookup(inode* dd, const char* name);
-int tree_lookup(const char* path);
-int directory_put(inode* dd, const char* name, int inum);
-int directory_delete(inode* dd, const char* name);
-slist_t* directory_list(const char* path);
-void print_directory(inode* dd);
+void directory_init();
+int directory_lookup(inode *di, const char *name);
+int directory_put(inode *di, const char *name, int inum);
+int directory_delete(inode *di, const char *name);
+slist_t *directory_list(const char *path);
+void print_directory(inode *dd);
 
 #endif
-
