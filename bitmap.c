28edd0ed9c6ab1891b768d7b9ea57b242a072129
trentwiles <wilest7@gmail.com>
2024-12-09 11:50:42 -0500

    refactor bitmaps

--- bitmap.c
+++ bitmap.c
@@ -1,32 +1,49 @@
-// Implementation of bitmap.h
-
-#include "bitmap.h"
+/**
+ * @file bitmap.c
+ * @author CS3650 staff
+ *
+ * Bitmap implementation.
+ */
+#include <stdint.h>
 #include <stdio.h>
 
-// gets the value at but ii
-int bitmap_get(void* bm, int ii) {
-    int jj = ii % 8;
+#include "bitmap.h"
 
-    return ((char*)bm)[ii / 8] >> jj & 1;
+#define nth_bit_mask(n) (1 << (n))
+#define byte_index(n) ((n) / 8)
+#define bit_index(n) ((n) % 8)
+
+// Get the given bit from the bitmap.
+int bitmap_get(void *bm, int i) {
+  uint8_t *base = (uint8_t *) bm;
+
+  return (base[byte_index(i)] >> bit_index(i)) & 1;
 }
 
-// puts a value at the specified bit in the bitmap
-void bitmap_put(void* bm, int ii, int vv) {
-    bitmap_print(bm, ii);
-    int bit = (vv) ? 1 : 0;
-    char* bitmap = (char*)bm;
-    if (bitmap_get(bm, ii) == bit) {
-        return;
-    }
-    else {
-        bitmap[(ii / 8)] ^= 1 << ii % 8; 
-    }
-    bitmap_print(bm, ii);
+// Set the given bit in the bitmap to the given value.
+void bitmap_put(void *bm, int i, int v) {
+  uint8_t *base = (uint8_t *) bm;
+
+  long bit_mask = nth_bit_mask(bit_index(i));
+
+  if (v) {
+    base[byte_index(i)] |= bit_mask;
+  } else {
+    bit_mask = ~bit_mask;
+    base[byte_index(i)] &= bit_mask;
+  }
 }
 
-// debug statement
-void bitmap_print(void* bm, int size) {
-    for (int ii = 0; ii < size * 4; ++ii) {
-        printf("%d", bitmap_get(bm, ii));
+// Pretty-print the bitmap (with the given no. of bits).
+void bitmap_print(void *bm, int size) {
+
+  for (int i = 0; i < size; i++) {
+    putchar(bitmap_get(bm, i) ? '1' : '0');
+
+    if ((i + 1) % 64 == 0) {
+      putchar('\n');
+    } else if ((i + 1) % 8 == 0) {
+      putchar(' ');
     }
+  }
 }
